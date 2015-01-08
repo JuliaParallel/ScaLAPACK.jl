d612529fc209fccc9183d0133610da86a5a3cf09
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2015-01-08 10:41:14 -0500

    Restructure and fix test for nprocs larger than grid points

--- src/blacs.jl
+++ src/blacs.jl
@@ -7,18 +7,18 @@ function blacs_pinfo()
     return mypnum[1], nprocs[1]
 end
 
-function blacs_gridinfo()
+function blacs_gridinfo(ictxt::Integer)
     nprow = Array(Int32, 1)
     npcol = Array(Int32, 1)
     myprow = Array(Int32, 1)
     mypcol = Array(Int32, 1)
     ccall((:blacs_gridinfo_, libscalapack), Void,
         (Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}),
-        ictxt, nprow, npcol, myprow, mypcol)
+        &ictxt, nprow, npcol, myprow, mypcol)
     return nprow[1], npcol[1], myprow[1], mypcol[1]
 end
 
-blacs_gridexit() = ccall((:blacs_gridexit_, libscalapack), Void, (Ptr{Int32},), ictxt)
+blacs_gridexit(ictxt::Integer) = ccall((:blacs_gridexit_, libscalapack), Void, (Ptr{Int32},), &ictxt)
 
 blacs_exit(cont = 0) = ccall((:blacs_exit_, libscalapack), Void, (Ptr{Int},), &cont)
 
--- test/test.jl
+++ test/test.jl
@@ -1,27 +1,39 @@
 using ScaLAPACK
+using MPI
+
+MPI.Init()
+
+Base.disable_threaded_libs()
 
 # problem size
-n = 5000
-nb = div(n, 2)
+n = 10000
+bf = 100
+nb = div(n, bf)
 
 # initialize grid
-ScaLAPACK.sl_init(2, 2)
+ic = ScaLAPACK.sl_init(8, 8)
 
 # who am I?
-nprow, npcol, myrow, mycol = ScaLAPACK.blacs_gridinfo()
+nprow, npcol, myrow, mycol = ScaLAPACK.blacs_gridinfo(ic)
+np = ScaLAPACK.numroc(n, nb, myrow, 0, nprow)
+nq = ScaLAPACK.numroc(n, nb, mycol, 0, npcol)
+println("myrow: $myrow, mycol: $mycol, nb: $nb, np: $np, nq: $nq")
 
-# Get DArray info
-dA = ScaLAPACK.descinit(n, n, nb, nb, 0, 0, nb)
+if nprow >= 0 && npcol >= 0
+    # Get DArray info
+    dA = ScaLAPACK.descinit(n, n, nb, nb, 0, 0, ic, np)
 
-# allocate local array
-A = randn(nb, nb)
+    # allocate local array
+    A = randn(int(np), int(nq))
 
-# calculate DSVD
-V, s, U = ScaLAPACK.pdgesvd!('N', 'N', n, n, A, 1, 1, dA, Array(Float64, n), Array(Float64, 0, 0), 0, 0, dA, Array(Float64, 0, 0), 0, 0, dA)
+    # calculate DSVD
+    V, s, U = ScaLAPACK.pdgesvd!('N', 'N', n, n, A, 1, 1, dA, Array(Float64, n), Array(Float64, 0, 0), 0, 0, dA, Array(Float64, 0, 0), 0, 0, dA)
 
-# show result
-myrow == 0 && mycol == 0 && println(s[1:3])
+    # show result
+    myrow == 0 && mycol == 0 && println(s[1:3])
 
-# clean up
-ScaLAPACK.blacs_gridexit()
+    # clean up
+    ScaLAPACK.blacs_gridexit(ic)
+end
+
 ScaLAPACK.blacs_exit()
